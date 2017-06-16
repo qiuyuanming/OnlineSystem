510eacc34aa01d4d9b717248ce91c5d1b195387d
nifflers <pay@nifflers.net>
2017-06-16 19:31:13 +0800

    fix bugs

--- src/java/com/qdu/service/ProductService.java
+++ src/java/com/qdu/service/ProductService.java
@@ -5,10 +5,40 @@
  */
 package com.qdu.service;
 
+import com.qdu.dao.ProductDao;
+import com.qdu.pojo.Product;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author nifflers
  */
+@Service
 public class ProductService {
-    
+
+    @Autowired
+    private ProductDao productDao;
+
+    public List getProductList() {
+        return productDao.getProductList();
+    }
+
+    public Product getProduct(int productID) {
+        return productDao.getProduct(productID);
+    }
+
+    public void addProduct(Product newProduct) {
+        productDao.insert(newProduct);
+    }
+
+    public void updateProduct(Product updateProduct) {
+        productDao.update(updateProduct);
+    }
+
+    public void detelteProduct(int productID) {
+        productDao.delete(productID);
+    }
+
 }
---
+++ src/java/com/qdu/service/UsersService.java
@@ -0,0 +1,53 @@
+
+package com.qdu.service;
+
+import com.qdu.dao.UsersDao;
+import com.qdu.pojo.Users;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+    @Autowired
+    private UsersDao usersDao;
+    
+    public List getUserList(){
+        return usersDao.getUserList();
+    }
+    
+    public Users getUser(String userId) {
+        return usersDao.getUser(userId);
+    }
+
+    public void addUser(Users newUser) {
+        usersDao.insert(newUser);
+    }
+
+    public void editUser(Users updatedUser) {
+        usersDao.update(updatedUser);
+    }
+
+    public void deleteUser(String userId) {
+        usersDao.delete(userId);
+    }
+    //验证登录
+    public Users validateUser(String userId,String password){
+        Users user=usersDao.getUser(userId);
+        if(user!=null&&user.getUserPassword().equals(password))
+            return user;
+        else
+            return null;
+    }
+    
+    //完成注册
+    public boolean registerUser(Users newUser){
+        try{
+            System.out.println(newUser.getUserAddress());
+            usersDao.insert(newUser);
+            return true;
+        }catch(Exception e){
+            return false ;
+        }
+    }
+}
