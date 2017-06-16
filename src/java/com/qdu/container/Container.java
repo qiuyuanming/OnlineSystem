0c328dbd92896a21907b592e4dc02b20ce3e3ea1
nifflers <pay@nifflers.net>
2017-06-16 19:25:00 +0800

    add Excel Container implement

---
+++ src/java/com/qdu/container/Container.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qdu.container;
+
+import jxl.write.Label;
+
+/**
+ *
+ * @author nifflers
+ */
+public class Container {
+    
+    //创建要显示的内容，创建一个单元格，第一个参数列坐标，第二个参数为行坐标，第三个参数为内容。
+    Label label = new Label(0,1,"string");
+}
