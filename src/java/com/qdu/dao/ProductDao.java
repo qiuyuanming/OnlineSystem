7a588878d581cecaea33511c6191ecc520159f14
nifflers <pay@nifflers.net>
2017-06-16 19:27:42 +0800

    update ProdcutDao add UserDao

--- src/java/com/qdu/dao/ProductDao.java
+++ src/java/com/qdu/dao/ProductDao.java
@@ -5,10 +5,67 @@
  */
 package com.qdu.dao;
 
+import com.qdu.pojo.Product;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author nifflers
  */
+@Repository
 public class ProductDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    //增加產品
+    public void insert(Product newProduct) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(newProduct);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void update(Product updateProduct) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(updateProduct);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(int productID) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(productID);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public Product getProduct(int productID) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Product product = (Product) session.get(Product.class, productID);
+        session.getTransaction().commit();
+        session.close();
+        return product;
+    }
     
+    public List getProductList(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        //Query query = session.cancelQuery("from product");
+        Query query = session.createQuery("from prodcut");
+        List list = query.list();
+        session.getTransaction().commit();
+        session.close();
+        return list;
+    }
+
 }
---
+++ src/java/com/qdu/dao/UsersDao.java
@@ -0,0 +1,60 @@
+
+package com.qdu.dao;
+
+import com.qdu.pojo.Users;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsersDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    public void insert(Users newUser){
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(newUser);
+        session.getTransaction().commit();
+        session.close();
+    }
+    
+    public void update(Users updatedUser){
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(updatedUser);
+        session.getTransaction().commit();
+        session.close();
+    }
+    
+    public void delete(String userId){
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        Object user=session.get(Users.class, userId);
+        session.delete(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+    
+    public Users getUser(String userId){
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        Users user=(Users)session.get(Users.class, userId);
+        session.getTransaction().commit();
+        session.close();
+        return user;
+    }
+    
+    public List getUserList(){
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        Query query=session.createQuery("from Users");
+        List list=query.list();
+        session.getTransaction().commit();
+        session.close();
+        return list;
+    }
+}
