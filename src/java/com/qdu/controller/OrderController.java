3f213279bf9f68e0ff39f424bfb5463e40b53cc3
nifflers <pay@nifflers.net>
2017-06-19 16:07:01 +0800

    add OrderController

---
+++ src/java/com/qdu/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.qdu.controller;
+
+import com.qdu.service.OrderService;
+import java.sql.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/orderlist")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    //根据日期显示用户
+    @RequestMapping(value = "/showAllUsers")
+    public String showAllUsers(Date time1, Date time2, ModelMap map) {
+        map.addAttribute("List", service.getUserListbyTime(time1, time2));
+        return "result";
+    }
+
+    @RequestMapping(value = "/show")
+    public String show(ModelMap map) {
+        map.addAttribute("cusList", service.getCustomerByTime());
+        return "cusresult";
+    }
+
+}
